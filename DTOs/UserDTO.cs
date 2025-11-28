8512d0b979ff0f2b3b2c4fd8686a2c0014a86397
Ash <reiashtray@gmail.com>
2025-11-28 17:44:40 +0100

    //wip

--- DTOs/UserDTO.cs
+++ DTOs/UserDTO.cs
@@ -2,6 +2,6 @@ namespace DTOs;
 
 public class UserDTO
 {
-    public required string UserName {get; set;}
-    public required string Password {get; set;}
+    public string UserName {get; set;}
+    public string Password {get; set;}
 }
